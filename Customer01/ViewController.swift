045ca0dccb9d8e375f532370aa69416d8729897c
Joyus Jose Mathew <joyus.jose@gmail.com>
2020-06-17 11:40:48 +0100

    Second Commit

--- Customer01/ViewController.swift
+++ Customer01/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let testVariable = 1
+        print(testVariable)
         // Do any additional setup after loading the view.
     }
 
